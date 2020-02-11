a8c813c989c26f2122e0002ac5d01d426f10c40d
rsteinhart <steinhartrachel@yahoo.ca>
2020-02-10 18:40:55 -0800

    start

--- HW3/hw3_v1.py
+++ HW3/hw3_v1.py
@@ -1,7 +1,7 @@
 # %%
 import pandas as pd
 import pylab as pl 
-
+import matplotlib.pyplot as plt
 
 # %% 
 # Take out all the rows with segment, rant and points 
@@ -14,13 +14,51 @@ for elem in files:
         for line in oldfile:
             if not any(bad_word in line for bad_word in bad_words):
                 newfile.write(line)
-# c = pd.read_csv('africa-cil-new.txt')
-# print(c)
+c = pd.read_csv('new-africa-cil.txt')
+print(c)
 # %%
-new_files = ['new-africa-cil.txt', 'new-asia-cil.txt', 'new-europe-cil.txt', 'new-namer-cil.txt', 'new-samer-cil.txt']
+df_total = pd.DataFrame({'lat': [ ], 'long': [ ]})
+new_files = ['new-africa-cil.txt','new-asia-cil.txt', 'new-europe-cil.txt', 'new-namer-cil.txt', 'new-samer-cil.txt']
+#df_africa = pd.read_csv('new-africa-cil.txt', sep=' ')
 for elem in new_files:
-    df = pd.read_csv(elem)
+    df = pd.read_csv(elem, sep=' ')
     df.columns = ['lat', 'long']
+    df_total = df_total.append(df)
+    #print(df_total)
+    #df_total.columns = ['lat', 'long']
 
 # %%
-new_files = ['new-africa-cil.txt', 'new-asia-cil.txt', 'new-europe-cil.txt', 'new-namer-cil.txt', 'new-samer-cil.txt']
+df_total.plot(x = 'long', y='lat', kind='scatter')
+# %%
+df_africa = pd.read_csv('new-africa-cil.txt', sep=' ')
+#print(df_africa)
+df_africa.columns = ['lat', 'long']
+
+df_asia = pd.read_csv('new-asia-cil.txt', sep=' ')
+df_asia.columns = ['lat', 'long']
+
+df_europe = pd.read_csv('new-europe-cil.txt', sep=' ')
+df_europe.columns = ['lat', 'long']
+
+df_namer = pd.read_csv('new-namer-cil.txt', sep=' ')
+df_namer.columns = ['lat', 'long']
+
+df_samer = pd.read_csv('new-samer-cil.txt', sep=' ')
+df_samer.columns = ['lat', 'long']
+
+df1 = df_africa.append(df_asia)
+df2 = df1.append(df_europe)
+#print(df2)
+df3 = df2.append(df_namer)
+df4 = df3.append(df_samer)
+print(df4)
+# %%
+df4.plot(x = 'long', y='lat', kind='scatter')
+
+# lat_coords = df4['lat']
+# long_coords = df4['long']
+# plt.scatter(long_coords,lat_coords)
+
+
+
+# %%
