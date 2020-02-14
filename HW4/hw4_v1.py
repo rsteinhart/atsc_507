32cae88a7963f00b69dfcd50ac1f91c874e47a28
Rachel Steinhart <rsteinhart@a23-150.eos.ubc.ca>
2020-02-13 16:49:20 -0800

    final first draft

---
+++ HW4/hw4_v1.py
@@ -0,0 +1,63 @@
+# %%
+import numpy as np
+# %%
+#Question setup
+Tref_0=2
+A=1
+c=1.5
+delta_t=1
+#t=m*delta_t
+
+T_ref = 2
+t=1 #?
+m = t/delta_t
+
+#t = np.linspace(0,1000,1000/delta_t)
+#T = A*(c*m*delta_t + Tref -Tref_0)*(Tref_0-c*m*delta_t)
+def slope(T,t):
+    delT_delt = 1.5*(2-1.5*t-(T/(2-1.5*t)))
+    return(delT_delt)
+
+# %%
+#Euler Forward
+delT_delt = slope(T_ref, t)
+T_n1 = delT_delt*delta_t + T_ref
+print('Euler Forward: T at the following timestep is', T_n1)
+
+# %%
+#RK2
+#delT_delt = slope(T_ref, t)
+T_star = T_ref + (delta_t/2)*slope(T_ref, t)
+#delTstar_delt = slope(T_star,t)
+T_n1_RK2 = T_ref + delta_t*slope(T_star,t)
+print('RK2: T at the following timestep is', T_n1_RK2)
+
+# %%
+#RK3
+T_star = T_ref + (delta_t/3)*slope(T_ref,t)
+T_2_star = T_ref + (delta_t/2)*slope(T_star,t)
+T_n1_RK3 = T_ref + delta_t*slope(T_2_star,t)
+print('RK3: T at the following timestep is', T_n1_RK3)
+
+# %%
+#RK4
+k1 = slope(T_ref,t)
+k2 = slope(T_ref+0.5*delta_t*k1,t+0.5*delta_t)
+k3 = slope(T_ref+0.5*delta_t*k2,t+0.5*delta_t)
+k4 = slope(T_ref + delta_t*k3,t+delta_t)
+T_n1_RK4 = T_ref + (delta_t/6)*(k1 + 2*k2 + 2*k3 + k4)
+print('RK4: T at the following timestep is', T_n1_RK4)
+
+# %%
+#Analytically
+T = A*(c*m*delta_t + T_ref -Tref_0)*(Tref_0-c*m*delta_t) 
+print('Analytically: T at the following timestep is', T)
+
+# %%
+# Compare results
+print('Eular forward:', T_n1)
+print('RK2:', T_n1_RK2)
+print('RK3:', T_n1_RK3)
+print('RK4:', T_n1_RK4)
+print('Analytical:', T)
+print('We can see that RK4 was the method that gave the closest answer to the analytical solution')
