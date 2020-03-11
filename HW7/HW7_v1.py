087b93d3c4af8bc7fb464e15348e638eb70072b9
Rachel Steinhart <rsteinhart@a23-150.eos.ubc.ca>
2020-03-11 16:56:34 -0700

    second upload

--- HW7/HW7_v1.py
+++ HW7/HW7_v1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 # %%
 # initial conditions
-imax = 1000 # number of grid points in x-direction
+imax = int(1000) # number of grid points in x-direction
 delx = 100 # horizontal grid spacing (m)
 delt = 10 # time increment (s)
 u = 5 # horizontal wind speed (m/s)
@@ -42,40 +42,59 @@ cideal[720:740] = np.linspace(0,-0.5*cmax,20)
 cideal[740:760] = np.linspace(-0.5*cmax,0,20)
 
 # %%
-# 4) Advect the concentration puff anomaly for the following number of time steps 
-# and plot in great the resulting concentrations using foward in time, backward in space
-
-j1 = np.zeros(imax+1)
-j1[0:1001] = np.linspace(0,1000,1001)
-j1 = j1.astype(int)
-
-nsteps = (imax-300)/(u*delt/delx)
-print(nsteps)
-t = np.zeros(int(nsteps))
-t[0:int(nsteps)] = np.linspace(0,int(nsteps)*delt,int(nsteps))
-
-# Forward in time backward in space
-n = 0
-#j = 1
-Conc_ftbs = np.zeros(nsteps,imax)
-#while n <= nsteps:
-for elem in t:
-    n = elem
-    for elem in j1[1:1001]:
-        j = elem
-        Conc_ftbs[j,n+1] = (delt/delx)*(Conc_ftbs[j,n]-Conc_ftbs[j-1,n]) + Conc_ftbs[j,n]
-        #n = n+1
-        #j = j+1
-#Conc_ftbs[0] = 0
-#Conc_ftbs = Conc_ftbs.T
-plt.plot(j1[1:1001],Conc_ftbs)
-plt.show()
-
-# %%
 # Plot
 plt.plot(x,cideal, 'r')
 plt.plot(x, conc, 'b')
 plt.legend('initial','ideal')
 plt.show()
+
+# %%
+# 4) Advect the concentration puff anomaly for the following number of time steps 
+# and plot in great the resulting concentrations using foward in time, backward in space
+
+# j1 = np.zeros(imax+1)
+# j1[0:1001] = np.linspace(0,1000,1001)
+# j1 = j1.astype(int)
+
+nsteps = (imax-300)/(u*delt/delx)
+nsteps = int(nsteps)
+print(nsteps)
+# t = np.zeros(int(nsteps))
+# t[0:int(nsteps)] = np.linspace(1,int(nsteps)*delt,int(nsteps))
+# t = t[1:int(nsteps)]
+
+# Forward in time backward in space
+s = (nsteps, imax)
+Conc_ftbs = np.zeros(s)
+Conc_ftbs[0,:] = conc
+# for elem in t:
+#     n = elem
+#     for elem in j1[1:1001]:
+#         j = elem
+#         Conc_ftbs[j,n+1] = (delt/delx)*(Conc_ftbs[j,n]-Conc_ftbs[j-1,n]) + Conc_ftbs[j,n]
+
+t=[0]
+time = 0
+while time < nsteps:
+    time = time+1
+    t = np.append(t,time)
+#print(t)
+
+x1 = [0]
+distance = 0
+while distance < imax:
+    distance = distance +1
+    x1 = np.append(x1,distance)
+#print(x1)
+
+for a in t:
+    for b in x1:
+        Conc_ftbs[a+1,b] = (delt/delx)*(Conc_ftbs[a,b]-Conc_ftbs[a,b-1]) + Conc_ftbs[a,b]
+
+Conc_ftbs_plt = Conc_ftbs[1400,:]
+plt.plot(x,Conc_ftbs_plt)
+plt.show()
+
+
 
 
