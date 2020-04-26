764216620c2b06ffe0dbc2fd1b3dcca0fa099567
rsteinhart <steinhartrachel@yahoo.ca>
2020-04-26 10:38:23 -0700

    done

--- HW10/HW10_v4.py
+++ HW10/HW10_v4.py
@@ -1,4 +1,9 @@
-# %% 
+# %% [markdown]
+# ATSC 507 HW10  
+# Rachel Steinhart  
+# April 20, 2020
+
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,7 +13,6 @@ import math
 pi = 3.14159
 # %%
 # Function defining polynomial
-
 def func_y(x):
     y = 0.5*x + x*np.sin(2*pi*x/10)
     return (y)
@@ -62,8 +66,8 @@ I4 = ((b-a)/2)*sum(w_k4*fxk4)
 # print(I4)
 
 m = 6
-xi_k6= np.array([0.3399810436,-0.3399810436, 0.8611363116, -0.8611363116])
-w_k6 = np.array([0.6521451549, 0.6521451549, 0.3478548451,0.3478548451])
+xi_k6= np.array([0.2386191861,-0.2386191861, 0.6612093865, -0.6612093865, 0.9324695142,-0.9324695142])
+w_k6 = np.array([0.4679139346, 0.4679139346, 0.3607615730,0.3607615730,0.1713244924,0.1713244924])
 xk6 = calc_xk(a,b,xi_k6)
 fxk6 = func_y(xk6)
 I6 = ((b-a)/2)*sum(w_k6*fxk6)
@@ -77,33 +81,36 @@ fxk8 = func_y(xk8)
 I8 = ((b-a)/2)*sum(w_k8*fxk8)
 # print(I8)
 
-table = {'m=2 I': [I2],
-'m=4 I': [I4],
-'m=6 I': [I6],
-'m=8 I': [I8]}
+table = {'m=2': [I2],
+'m=4': [I4],
+'m=6': [I6],
+'m=8': [I8]}
 
 df = pd.DataFrame(table)
 print(df)
 
-# %%
-# Gaussian quadrature reaches the exact solution for polynomials of 2n-1. We can see that 
-# for m=8, the solution agrees with the analytical solution.
+# %% [markdown]
+# Gaussian quadrature reaches the exact solution for polynomials of 2n-1. We can see that for m=8, the solution agrees with the analytical solution. An increase in n/m will provide a more accurate solution because the function is fit more closely with each added point.
+
+# %% [markdown]
+# Q2) Triangular Spectral Truncation is used with highest order N=1279. 
+# Meaning that there are 1280 truncation points.
+# Warner has the same maximum wavenumber of 1279. 
+#
+# Warner's four equations are:
+# $$ L_{1}= \frac{2\pi a}{3K + 1},   
+# L_{2} =\frac{\pi a}{K},  L_{3}=\frac{2\sqrt{\pi}a}{K+1},
+# L_{4}=\frac{\sqrt{2}\pi a}{K} $$ 
+#  
+# With a spectral resolution of 799 L1 = 16.7km, L2 = 25.0 km, L3 = 28.2, L4 = 35.4km
+# ECMWF uses this equation:
+# $$ L=\frac{2\pi a}{n} $$ 
+#
+# with the same spectral resolution of 799 L = 50.10km
+# The ECMWF equation is most similar to L2.
 
 # %%
-#Q2) Triangular Spectral Truncation is used with highest order N=1279
-# whereas Warner uses resultion of 799
-
-# %% 
 # Q3a)
-
-# def calc_P(m,n,mu):
-#     term1 = np.sqrt((2*n+1)*gamma(int(n-m+1))/gamma(int(n+m+1)))
-#     term2 = ((1-mu**2)**(m/2))/((2**n)*gamma(int(n+1)))
-#     term3 = (d**(n+m))/(d*mu**(n+m))
-#     term4 = find_eq(m,n,mu)
-
-#     P = term1*term2*term3*term4
-#     return P
 
 def find_eq(m,n,mu):
     if m==0:
@@ -141,7 +148,7 @@ def find_eq(m,n,mu):
             deriv = (19958400*mu**4)-(10886400*mu**2)+604800
 
     return deriv
-            
+
 # %%
 m_a=0
 n_a=np.array(np.linspace(1,4,4))
@@ -149,9 +156,8 @@ mu = np.array(np.linspace(-1,1,100))
 Pmn_a = np.zeros((len(n_a),len(mu)))
 
 for n in n_a:
-    # Pmn = calc_P(m_a,elem,mu)
-    term1 = np.sqrt((2*+1)*math.factorial(int(n-m_a))/math.factorial(int(n+m_a)))
-    term2 = ((1-mu**2)**(m/2))/((2**n)*math.factorial(int(n)))
+    term1 = np.sqrt((2*n+1)*math.factorial(int(n-m_a))/math.factorial(int(n+m_a)))
+    term2 = ((1-mu**2)**(m_a/2))/((2**n)*math.factorial(int(n)))
     term3 = find_eq(m_a,n,mu)
 
     P = term1*term2*term3
@@ -164,7 +170,7 @@ plots = ax.plot(mu,Pmn_a.T)
 ax.set_xlabel('mu')
 ax.set_ylabel('P(mu)')
 ax.legend(plots,labels)
-plt.title('Q3 part a (m=0)')
+plt.title('Q3 part a (m=0)');
 
 # %%
 # Q3b)
@@ -173,13 +179,13 @@ n_b=np.array(np.linspace(1,5,5))
 Pmn_b = np.zeros((len(n_b),len(mu)))
 
 for n in n_b:
-    # Pmn = calc_P(m_a,elem,mu)
-    term1 = np.sqrt((2*+1)*math.factorial(int(n-m_b))/math.factorial(int(n+m_b)))
-    term2 = ((1-mu**2)**(m/2))/((2**n)*math.factorial(int(n)))
+    term1 = np.sqrt((2*n+1)*math.factorial(int(n-m_b))/math.factorial(int(n+m_b)))
+    term2 = ((1-mu**2)**(m_b/2))/((2**n)*math.factorial(int(n)))
     term3 = find_eq(m_b,n,mu)
 
     P = term1*term2*term3
     Pmn_b[int(n)-1] = P
+    
 labels =['n=1','n=2','n=3','n=4','n=5']
 
 fig, ax=plt.subplots(1,1)
@@ -187,7 +193,7 @@ plots = ax.plot(mu,Pmn_b.T)
 ax.set_xlabel('mu')
 ax.set_ylabel('P(mu)')
 ax.legend(plots,labels)
-plt.title('Q3 part b (m=1)')
+plt.title('Q3 part b (m=1)');
 
 # %%
 # Q3c)
@@ -196,8 +202,8 @@ n_c=np.array(np.linspace(2,6,5))
 Pmn_c = np.zeros((len(n_c),len(mu)))
 
 for n in n_c:
-    term1 = np.sqrt((2*+1)*math.factorial(int(n-m_c))/math.factorial(int(n+m_c)))
-    term2 = ((1-mu**2)**(m/2))/((2**n)*math.factorial(int(n)))
+    term1 = np.sqrt((2*n+1)*math.factorial(int(n-m_c))/math.factorial(int(n+m_c)))
+    term2 = ((1-mu**2)**(m_c/2))/((2**n)*math.factorial(int(n)))
     term3 = find_eq(m_c,n,mu)
 
     P = term1*term2*term3
@@ -210,7 +216,7 @@ plots = ax.plot(mu,Pmn_c.T)
 ax.set_xlabel('mu')
 ax.set_ylabel('P(mu)')
 ax.legend(plots,labels)
-plt.title('Q3 part c (m=2)')
+plt.title('Q3 part c (m=2)');
 
 # %%
 # Q4)
